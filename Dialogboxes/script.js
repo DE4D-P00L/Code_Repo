b3ff8ad8b495e0b8b54b932bb75ce11d0ceaf025
Prashant <prashant200025@gmail.com>
2021-11-10 15:01:43 +0530

    Added Dialog boxes

---
+++ Dialogboxes/script.js
@@ -0,0 +1,29 @@
+var database = {
+    id:"ABCD",
+    pass:"1234" 
+};
+
+var counter = 1;
+
+while (true){
+    var UId = prompt("Enter User Id","ID");
+    var UPass = prompt("Enter Password","Password");
+
+    if(UId == database.id && UPass == database.pass){
+        document.write("<H1>Welcome</H1>");
+        break;
+    }
+
+    else{
+        if(counter >= 3){
+            alert("You failed 3 times");
+            break;
+        }
+        else{
+            if(!confirm("Want to try again?")){
+                break;
+            }
+        }
+    }
+    counter++;
+}
