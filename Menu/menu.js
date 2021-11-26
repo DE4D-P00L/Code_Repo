497da4fb1242621a6828b38573d558e0a751654c
Prashant <prashant200025@gmail.com>
2021-11-26 19:41:24 +0530

    Modified Menu page

--- Menu/menu.js
+++ Menu/menu.js
@@ -10,5 +10,26 @@ btn.addEventListener("click", ()=>{
     }
 })
 
+const navbar = document.querySelector('.menu');
+const hm = document.querySelector('.menu_');
+const hmb = document.querySelectorAll('span');
+console.log(hmb);
+
+window.onscroll = () => {
+    if (window.scrollY > 500) {
+        navbar.style.backgroundColor = "#333";
+        hm.style.backgroundColor = "#000";
+        hmb.forEach((bar) => {
+            bar.style.backgroundColor = "#fff";
+        });
+    } else {
+        navbar.style.backgroundColor = "#ffffff1a";
+        hm.style.backgroundColor = "#fff";
+        hmb.forEach((bar) => {
+            bar.style.backgroundColor = "#000";
+        });
+    }
+};
+
 
 
